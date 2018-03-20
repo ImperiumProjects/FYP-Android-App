95058644b30204db56f8409234220f920754bad9
ImperiumProjects <jasonjp2@hotmail.com>
2018-03-20 22:46:45 +0000

    Modified HTTP Connection code to pull prediction percentage and display in app

--- app/src/main/java/com/example/imperium/finalyearproject/PredictionActivity.java
+++ app/src/main/java/com/example/imperium/finalyearproject/PredictionActivity.java
@@ -1,40 +1,26 @@
 package com.example.imperium.finalyearproject;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Config;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
-import java.util.ArrayList;
 
 public class PredictionActivity extends AppCompatActivity {
 
     private TextView predictionPercentage;
     private TextView colourCodeText;
     private CheckBox damSimulationCheckbox;
-    private JSONArray result;
+    private String predictionResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,18 +32,6 @@ public class PredictionActivity extends AppCompatActivity {
         damSimulationCheckbox = (CheckBox) findViewById(R.id.damSimulationCheckBox);
 
         new JSONAsyncTask().execute();
-        int num = Integer.parseInt((String)predictionPercentage.getText());
-        if(num < 30){
-            colourCodeText.setTextColor(Color.parseColor("#06840a"));
-            colourCodeText.setText("Green ");
-        } else if (num >= 30 & num < 80){
-            colourCodeText.setTextColor(Color.parseColor("#edb10e"));
-            colourCodeText.setText("Amber ");
-        }
-        else{
-            colourCodeText.setTextColor(Color.parseColor("#c90c0c"));
-            colourCodeText.setText("Red ");
-        }
     }
 
     public void updatePrediction(View view) {
@@ -79,11 +53,13 @@ public class PredictionActivity extends AppCompatActivity {
         protected Boolean doInBackground(String... urls) {
             try {
                 //Log.d("HTTP", "Making http request");
-                URL url = new URL("http://connect.bakguicraft.com:5000/prediction");
+                URL url = new URL("http://connect.bakguicraft.com:5000/test");
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
-                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                    //Log.d("HTTP", urlConnection.getResponseMessage());
+                    predictionResult = readStream(urlConnection.getInputStream());
+                    predictionResult = predictionResult.replaceAll("[^-?0-9]+", " ");
+                    predictionResult = predictionResult.replaceAll("\\s+","");
+                    Log.v("OUTPUT", predictionResult);
                 } catch (IOException e) {
                     System.out.println(e);
                 } finally {
@@ -92,38 +68,49 @@ public class PredictionActivity extends AppCompatActivity {
                 return true;
 
             } catch (Exception e) {
-                //Log.d("HTTP", e.toString());
                 System.out.println(e);
             }
-
-            /*try {
-                Log.d("HTTP", "Making php request");
-                URL url = new URL("http://connect.bakguicraft.com:5000/php_python");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                    Log.d("HTTP", urlConnection.getResponseMessage());
-                    while(in.available() > 0){
-                        char c = (char)in.read();
-                        Log.d("OUTPUT", "Char: " + c);
-                    }
-                } catch (IOException e) {
-                    System.out.println(e);
-                } finally {
-                    urlConnection.disconnect();
-                }
-                return true;
-
-
-            } catch (Exception e) {
-                Log.d("HTTP", e.toString());
-                System.out.println(e);
-            }*/
             return false;
         }
 
         protected void onPostExecute(Boolean result) {
             super.onPostExecute(result);
+            predictionPercentage.setText(predictionResult);
+            int num = Integer.parseInt((String)predictionPercentage.getText());
+            if(num < 30){
+                colourCodeText.setTextColor(Color.parseColor("#06840a"));
+                colourCodeText.setText("Green ");
+            } else if (num >= 30 & num < 80){
+                colourCodeText.setTextColor(Color.parseColor("#edb10e"));
+                colourCodeText.setText("Amber ");
+            }
+            else{
+                colourCodeText.setTextColor(Color.parseColor("#c90c0c"));
+                colourCodeText.setText("Red ");
+            }
         }
+    }
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
     }
 }
