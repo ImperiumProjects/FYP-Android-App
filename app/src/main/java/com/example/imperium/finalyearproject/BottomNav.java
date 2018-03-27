a30bf9acbb5aec4cd0448d23b2da32e453745c01
ImperiumProjects <jasonjp2@hotmail.com>
2018-03-27 17:38:30 +0100

    Created BottomNavBar, imported nav_item icons and created Fragments for nav bar

---
+++ app/src/main/java/com/example/imperium/finalyearproject/BottomNav.java
@@ -0,0 +1,13 @@
+package com.example.imperium.finalyearproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BottomNav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav);
+    }
+}
